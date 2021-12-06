5943cb0e8991223492efee1b686f6dde1f6cec28
Anthony Bacilio Ruiz <bacilio.anthony@gmail.com>
2021-12-05 22:52:07 -0500

    ya esta js

--- js/ordenes.js
+++ js/ordenes.js
@@ -1,3 +1,20 @@
-btneliminarorden.addeventlistener("click", () => {
+btneliminarorden.addEventListener("click", () => {
     modaleliminar.style.display="flex";
+})
+
+volveraordenes.addEventListener("click", () => {
+    modaleliminar.style.display="none";
+})
+
+eliminarorden.addEventListener("click", () => {
+    ordenaeliminar.style.display="none";
+    modaleliminar.style.display="none";
+})
+
+btnseguirpedido.addEventListener("click", ()=>{
+    modallogistica.style.display="flex";
+})
+
+btnregresarordenes.addEventListener("click", ()=>{
+    modallogistica.style.display="none";
 })
